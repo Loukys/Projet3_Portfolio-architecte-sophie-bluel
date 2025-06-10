6f8f5acad86ebfac97eb13cd8580003ea5c3741d
Guillaume Louail <louailguillaume77400@gmail.com>
2025-06-10 10:50:12 +0200

    suppression des fichiers inutiles et try/catch ajouté au main.js

--- FrontEnd/js/main.js
+++ FrontEnd/js/main.js
@@ -4,12 +4,17 @@ import { initMainPage } from './dom.js';
 import { clickFilters, logout, clickModif, setupValidateBtn, clickAddP } from './events.js';
 import { tokenLog } from './utils.js';
 
-document.addEventListener('DOMContentLoaded', () => {
-    initMainPage();
-    clickFilters();
-    logout();
-    clickModif();
-    setupValidateBtn();
-    clickAddP();
-    tokenLog();
-});
+try {
+    document.addEventListener('DOMContentLoaded', () => {
+        initMainPage();
+        clickFilters();
+        logout();
+        clickModif();
+        setupValidateBtn();
+        clickAddP();
+        tokenLog();
+    });
+}
+catch(error) {
+    console.error("Erreur lors du fonctionnement du site:", error);
+}
